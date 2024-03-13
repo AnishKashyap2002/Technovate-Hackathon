159ccc33bfd6b600ad62ef3d846492963290653c
Symphony-1 <aamitaswal02@gmail.com>
2024-03-13 19:06:42 +0530

    timeline arr

---
+++ src/constants/timeline.js
@@ -0,0 +1,65 @@
+import img1 from '../../public/timeline/1rs.webp'
+
+const timelineArr = [
+    {
+        title:"RedSkull ",
+        data:"Johann Schmidt, a ruthless Nazi leader, craved ultimate power. He experimented on himself with an unstable version of the Super Soldier Serum, transforming him into the Red Skull. Fueled by hatred and a thirst for power, Red Skull fixated on the Tesseract, a cosmic cube containing the Space Infinity Stone.  In 1942, HYDRA, the Nazi science division Red Skull led, located the Tesseract in Norway. However, Red Skull's attempts to harness its power would be repeatedly foiled by Captain America.",
+        image:"../../public/timeline/1rs.webp",
+        date:"1942"
+    },
+    {
+        title:"Steve Roger turned into Captain America",
+        data:"Despite his frail physique and constant rejections, Steve Rogers' unwavering determination to fight for his country in World War II led him to volunteer for Project Rebirth.  This innovative program, designed to create super soldiers, saw Dr. Erskine choose Steve not for his body but for his noble spirit.  The Super Soldier Serum transformed Steve's scrawny frame into a peak physical specimen, granting him enhanced strength, speed, and reflexes.  Now a symbol of American ideals, Steve donned the mantle of Captain America, wielding a vibranium shield.  Fighting alongside his friend Bucky Barnes, Captain America became a powerful force against the Nazi threat, leading missions to dismantle the evil Hydra organization.",
+        image:"../../public/timeline/2ca.jpg",
+        date:"1942"
+    },
+    {
+        title:"Black Widow was trained in red room",
+        data:"Natasha Romanoff's childhood vanished within the brutal confines of the Red Room. Torn from her family at a young age, she was conditioned to become a weapon – the ultimate Black Widow.  Her days were a relentless cycle of physical and mental manipulation. Grueling physical training honed her body into a lethal instrument, mastering various fighting styles and weaponry. But the Red Room wasn't just about physical prowess. Psychological manipulation fractured her sense of identity, replacing it with unwavering obedience and a thirst for completion. Memories were tampered with, replaced with fabricated narratives to ensure absolute loyalty.  Natasha endured this harrowing reality alongside other young girls, a twisted camaraderie forming amongst them, even as they were pitted against each other. This harsh training program aimed to forge emotionless assassins, devoid of personal attachments and programmed to fulfill any mission, no matter how deadly.",
+        image:"../../public/timeline/3bw.avif",
+        date:"1984"
+    },
+    {
+        title:"Carol Danvers became Captain Marvel",
+        data:"Carol Danvers' path to becoming Captain Marvel was a fiery one. Serving as a pilot for the US Air Force, Carol found herself caught in the blast from an experimental light-speed engine co-developed with the alien Kree race. The explosion fused her DNA with the Kree hero Mar-Vell's energy signature. This unexpected event unlocked extraordinary abilities within Carol. She gained superhuman strength, the power to fly, and the ability to project powerful energy blasts. Initially struggling to control these newfound abilities, Carol honed them through rigorous training with the Kree. However, her past life and memories remained fragmented due to the incident. As she wrestled with her identity, Carol embraced her cosmic connection and donned the mantle of Captain Marvel. Now a protector not just of Earth but galaxies beyond, Captain Marvel soars through the stars, a beacon of courage and unwavering resolve.",
+        image:"../../public/timeline/4cm.jpg",
+        date:"1995"
+    },
+    {
+        title:"",
+        data:"",
+        image:"../../public/timeline/",
+        date:""
+    },
+    {
+        title:"",
+        data:"",
+        image:"../../public/timeline/",
+        date:""
+    },
+    {
+        title:"",
+        data:"",
+        image:"../../public/timeline/",
+        date:""
+    },
+    {
+        title:"",
+        data:"",
+        image:"../../public/timeline/",
+        date:""
+    },
+    {
+        title:"",
+        data:"",
+        image:"../../public/timeline/",
+        date:""
+    },
+    {
+        title:"",
+        data:"",
+        image:"../../public/timeline/",
+        date:""
+    },
+    
+]
