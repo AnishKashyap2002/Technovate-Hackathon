31138c28680900227dfab6eaf00e8d1b55831e94
Kashish Sahu <125540624+gitKashish@users.noreply.github.com>
2024-03-14 08:43:01 +0530

    Navbar UI rework

--- src/components/poll-components/PollButton.tsx
+++ src/components/poll-components/PollButton.tsx
@@ -12,12 +12,7 @@ const PollButton = () => {
                 className="flex justify-center text-white text-3xl items-center gap-2 size-16 font-semibold rounded-lg border border-transparent bg-gradient-radial from-violet-600 to-indigo-600 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                 <IoMdAdd />
             </button>
-            {/* <div
-                className="text-white text-3xlshadow-md cursor-pointer w-fit bg-rose-500 rounded-full p-3"
-                onClick={() => setIsOpen(true)}
-            >
-                
-            </div> */}
+            
             <div className="bg-gray-600 px-2 py-2 invisible group-hover/parent:visible group-hover/parent:translate-x-0 transition translate-x-32 text-white text-sm rounded-md">
                 Create Poll
             </div>
