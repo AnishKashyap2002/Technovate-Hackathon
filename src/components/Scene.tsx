cbf3a006552ad1223facee5c552f48e32a5917db
anishkashyap2002 <anishkashyap311@gmail.com>
2024-03-15 09:27:23 +0530

    bug fix

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -16,32 +16,31 @@ const ModelArray = [<Model />, <ArcReactor />, <HydraHelmet />, <Shield />];
 
 const Scene = ({ i }: { i: number }) => {
     return (
-        <>
-            <Canvas
-                shadows
-                style={i == 0 ? { height: "100vh" } : { height: "400px" }}
-                className="absloute w-full z-20 "
-                dpr={[1, 2]}
-                camera={{ position: [0, 0, 4], fov: 50 }}
-            >
-                <ambientLight intensity={0.7} />
-                <spotLight
-                    intensity={0.5}
-                    angle={0.1}
-                    penumbra={1}
-                    position={[10, 15, 10]}
-                    castShadow
-                />
-                <Suspense fallback={null}>
-                    {ModelArray[i]}
-                    <Environment preset="city" />
-                </Suspense>
-                <OrbitControls
-                    autoRotate
-                    enableZoom={false}
-                />
-            </Canvas>
-        </>
+        <Canvas
+            key={Math.random()}
+            shadows
+            style={i == 0 ? { height: "100vh" } : { height: "400px" }}
+            className="absloute w-full z-20 "
+            dpr={[1, 2]}
+            camera={{ position: [0, 0, 4], fov: 50 }}
+        >
+            <ambientLight intensity={0.7} />
+            <spotLight
+                intensity={0.5}
+                angle={0.1}
+                penumbra={1}
+                position={[10, 15, 10]}
+                castShadow
+            />
+            <Suspense fallback={null}>
+                {ModelArray[i]}
+                <Environment preset="city" />
+            </Suspense>
+            <OrbitControls
+                autoRotate
+                enableZoom={false}
+            />
+        </Canvas>
     );
 };
 
